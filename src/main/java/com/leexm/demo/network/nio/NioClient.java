6dd877243fbcb3a29cc7545d6665fd5d8eb04351
lxm <leexm9@163.com>
2019-10-25 22:46:38 +0800

    nio 实现

---
+++ src/main/java/com/leexm/demo/network/nio/NioClient.java
@@ -0,0 +1,51 @@
+package com.leexm.demo.network.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author lxm
+ * @date 2019/10/25 13:09
+ */
+public class NioClient {
+
+    private static final int PORT = 8088;
+
+    private static final String HOST = "127.0.0.1";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int count = 5;
+        ByteBuffer message = ByteBuffer.wrap("Hello, world!".getBytes("UTF-8"));
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+
+        for (int i = 0; i < count; i++) {
+            new Thread(() -> {
+                try {
+                    SocketChannel socketChannel = SocketChannel.open();
+                    socketChannel.connect(new InetSocketAddress(HOST, PORT));
+
+                    socketChannel.write(message);
+                    socketChannel.shutdownOutput();
+
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    socketChannel.read(byteBuffer);
+                    byteBuffer.flip();
+                    String response = new String(byteBuffer.array(), 0, byteBuffer.limit(), "UTF-8");
+                    System.out.println(String.format("[%s]线程服务端响应:%s", Thread.currentThread().getName(), response));
+                    socketChannel.close();
+
+                    countDownLatch.countDown();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }, "Sender-" + i).start();
+        }
+
+        countDownLatch.await();
+        System.out.println(String.format("%d 个请求收发完成!", count));
+    }
+
+}
---
+++ src/main/java/com/leexm/demo/network/nio/NioServer.java
@@ -0,0 +1,70 @@
+package com.leexm.demo.network.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * nio实现服务端多线程版本
+ *
+ * @author lxm
+ * @date 2019/10/25 13:09
+ */
+public class NioServer {
+
+    private static final int PORT = 8088;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+        serverChannel.bind(new InetSocketAddress(PORT));
+
+        Selector selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        System.out.println("服务器启动，等待客户端连接...");
+        while (true) {
+            if (selector.select() <= 0) {
+                continue;
+            }
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = keys.iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+
+                if (!key.isValid()) {
+                    key.channel().close();
+                }
+
+                if (key.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else if (key.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    socketChannel.read(byteBuffer);
+                    byteBuffer.flip();
+                    String message = new String(byteBuffer.array(), 0, byteBuffer.limit(), "UTF-8");
+                    System.out.println(String.format("线程%s，收到客户端信息:%s", Thread.currentThread().getName(), message));
+
+                    message = String.format("[%s]", message);
+                    byteBuffer.clear();
+                    byteBuffer.put(message.getBytes("UTF-8"));
+                    byteBuffer.flip();
+                    socketChannel.write(byteBuffer);
+                    socketChannel.close();
+                }
+                iter.remove();
+            }
+        }
+    }
+
+}
